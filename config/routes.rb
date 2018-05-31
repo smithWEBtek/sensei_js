d1ab274e98879a371c67450cdfbcbd2004fb545d
johnwojo <jwojo13@gmail.com>
2018-05-31 15:22:11 -0400

    Added SSL cert. Added sessions controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,22 @@
-Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "application#index"
+# Rails.application.routes.draw do
+#   get 'sessions/create'
+#   get 'sessions/destroy'
+#   get 'home/show'
+#   devise_for :users
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#   root to: "application#index"
+#   end
+
+
+  Sensei::Application.routes.draw do
+    get 'auth/:provider/callback', to: 'sessions#create'
+    get 'auth/failure', to: redirect('/')
+    get 'signout', to: 'sessions#destroy', as: 'signout'
+    get 'sessions/create'
+    get 'sessions/destroy'
+    get 'home/show'
+    devise_for :users
+    resources :sessions, only: [:create, :destroy]
+    resource :home, only: [:show]
+    root to: "application#index"
   end
