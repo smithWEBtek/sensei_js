99719718ebb403acd4d81b8ce5d681682cfa9b6a
johnwojo <jwojo13@gmail.com>
2018-05-31 21:21:40 -0400

    Added env to the gemfile and moved secret and key to the env.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,3 +4,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, '962223945781-h3v9ucjt1vcj9n7cjfkur768necr16la.apps.googleusercontent.com', 'srttsejf9L1UgJ0lJitqn1zp',
   {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+ provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
+end
