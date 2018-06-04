35ea9e5be07a2aad9c3093067c16e5216eb03d8b
johnwojo <jwojo13@gmail.com>
2018-06-03 20:09:48 -0400

    Added current_user, signed_in?, and current_user= helper methods.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,20 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
   before_action :authenticate_user!
 
+    def current_user
+      @current_user ||= User.find_by_id(session[:user_id])
+    end
 
- def current_user
-   @current_user ||= User.find(session[:user_id]) if session[:user_id]
- end
+    def signed_in?
+      !!current_user
+    end
+
+    helper_method :current_user, :signed_in?
+
+    def current_user=(user)
+      @current_user = user
+      session[:user_id] = user.id
+    end
 
   def index
 
